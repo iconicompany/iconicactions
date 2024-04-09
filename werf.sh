dd42d8ddebf99191fa42230fba0f9e51db84c04b
Viacheslav Borisov <slavb18@iconicompany.com>
2024-04-09 11:32:51 +0400

    add Env to build time

--- werf.sh
+++ werf.sh
@@ -1,5 +1,5 @@
 DOCKER_BUILDKIT=true \
 WERF_ENV=testing \
-WERF_SET_DOMAIN=env.DOMAIN=main.iconicompany.icncd.ru \
+WERF_SET_DOMAIN=env.DOMAIN=iconicactions-main.iconicompany.icncd.ru \
 WERF_REPO=ghcr.io/iconicompany/iconicactions/iconicactions \
 werf $*
