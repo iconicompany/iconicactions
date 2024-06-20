e7a57b40a28bc5247ad792b0ad007ec201ef7c92
Viacheslav Borisov <slavb18@iconicompany.com>
2024-06-20 13:38:46 +0400

    ### Update deployment workflow and sanitize sensitive info
    Replaced project reference in README and install script for consistency with the iconicompany repository. Commented out sensitive database configuration details in werf.yaml to improve security and manageability.

--- README.md
+++ README.md
@@ -16,4 +16,4 @@ cat $HOME/.werf/global_secret_key | gh secret set WERF_SECRET_KEY
 ## Example usage
 
 See .github/workflows/deployment-*.yaml
-Copy to project, replace `uses: iconicompany/werfactions/.github/workflows/deployment.yml@main`
+Copy to project, replace `uses: iconicompany/iconicactions/.github/workflows/deployment.yml@main`
--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@ unzip $WORK_DIR/iconicactions.zip -d $WORK_DIR
 TARGET_PROJECT=$(basename $(readlink -f .))
 grep -Rl iconicactions $WORK_DIR/iconicactions-main|xargs sed -i "s/iconicactions/$TARGET_PROJECT/g"
 mkdir -p .github/workflows/
+sed -i 's^./.github/workflows/deployment.yml^iconicompany/iconicactions/.github/workflows/deployment.yml@main^' $WORK_DIR/iconicactions-main/.github/workflows/deployment-*.yml
 cp $WORK_DIR/iconicactions-main/.github/workflows/deployment-*.yml .github/workflows/
 cp -r $WORK_DIR/iconicactions-main/{.helm,werf.yaml,werf.sh,Dockerfile} .
