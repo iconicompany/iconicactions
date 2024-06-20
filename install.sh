a0465cf9b63fcd63d9d60ab21a46e2e4b7d51dc4
Viacheslav Borisov <slavb18@iconicompany.com>
2024-06-20 13:31:16 +0400

    install script

---
+++ install.sh
@@ -0,0 +1,8 @@
+WORK_DIR=`mktemp -d`
+curl -Lo $WORK_DIR/iconicactions.zip https://github.com/iconicompany/iconicactions/archive/refs/heads/main.zip
+unzip $WORK_DIR/iconicactions.zip -d $WORK_DIR
+TARGET_PROJECT=$(basename $(readlink -f .))
+grep -Rl iconicactions $WORK_DIR/iconicactions-main|xargs sed -i "s/iconicactions/$TARGET_PROJECT/g"
+mkdir -p .github/workflows/
+cp $WORK_DIR/iconicactions-main/.github/workflows/deployment-*.yml .github/workflows/
+cp -r $WORK_DIR/iconicactions-main/{.helm,werf.yaml,werf.sh,Dockerfile} .
